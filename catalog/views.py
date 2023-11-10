30ae8894af395c3c58de5a570a09f7fc10a15fac
Andrew <andbushuev@yandex.ru>
2023-11-10 13:48:05 +0500

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+
